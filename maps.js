27ef650970b4fb3c6988d271c6b6323b6fdd0ef4
Khusaeri <akhus22@gmail.com>
2019-08-23 09:36:24 +0700

    scrolling images gallery

--- maps.js
+++ maps.js
@@ -3,11 +3,17 @@ const http = require('http');
 
 (() => {
 	const SITE_LINK = process.argv[2];
+	var isDebug = false;
 	
 	if(!SITE_LINK) {
 		console.log('Usage : node maps.js google-map-url');
 		return;
 	}
+	
+	if(process.argv[3] == '--debug') {
+		isDebug = true;
+	}
+	
 	
 	const getTotalPhoto = () => {
 		let xpathExpression = "//span[contains(@class, 'gm2-body-2') and string-length(text()) > 0]",
@@ -18,10 +24,31 @@ const http = require('http');
 		return totalPhoto;
 	};
 	
+	const scrollGallery = async () => {
+		const gallery = document.querySelector('.section-layout.section-scrollbox.scrollable-y.scrollable-show'),
+				loading = document.querySelector('.section-loading-spinner');
+		let scrolling = true ;
+		
+		while(scrolling) {
+			gallery.scrollTo(0, gallery.scrollHeight);
+			await new Promise(resolve => setTimeout(resolve, 1000));
+			
+			scrolling = loading.parentElement.classList.contains('noprint') ;
+						
+			console.log(scrolling);
+			console.log(gallery.scrollHeight);
+		}
+	}
+	
 	(async () => {
-		const browser = await puppeteer.launch({headless : false});
+		const browser = await puppeteer.launch({headless : !isDebug});
 		const page = await browser.newPage();
 		await page.goto(SITE_LINK);
+		
+		if(isDebug) {
+			// show browser console on terminal
+			page.on('console', consoleObj => console.log(consoleObj.text()));
+		}
 		await page.waitForSelector('.section-image-pack-image-container');
 		
 		await page.evaluate(() => {
@@ -30,7 +57,9 @@ const http = require('http');
 		// await page.click('.section-image-pack-image-container'); // error on ubuntu server 16.04
 		
 		await page.waitForSelector('.gallery-image-high-res.loaded');
-		await page.waitFor(500); // wait for more than 1 gallery image loaded
+		await page.evaluate(scrollGallery);
+		
+		// await page.waitFor(500); // wait for more than 1 gallery image loaded
 		
 		const img = await page.evaluate(() => {
 			const gallery = document.querySelectorAll('.gallery-image-high-res.loaded');
@@ -42,11 +71,10 @@ const http = require('http');
 				imgs.push(url);
 			});
 			
-			console.log(imgs);
 			return imgs;
 		});
 		
-		console.log(JSON.stringify(img));
+		console.log(img);
 		await browser.close();
 	})();
 })();
